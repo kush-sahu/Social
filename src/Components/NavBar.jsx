5059ccdeed7d658e1a1a450799aa60c31bc3de7e
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2025-01-19 12:26:17 +0530

    update1.2

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,7 +6,8 @@ function NavBar({value}) {
   return (
     <div style={{ textAlign: "center", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "20px", backgroundColor: "rgb(5 20 38)" }}>
       <h1 style={{ color: "white" }}>
-        Document Analyzer
+      Social Media Content Analyzer
+
       </h1>
 
       { value && (<button className='Uploadbtn'  onClick={() => navigate("/")}>
